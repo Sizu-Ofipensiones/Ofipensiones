6cd3f0c802da3e0badb9341e6d66eda85b532f1a
I-Bonnett-I <t.bonnett@uniandes.edu.co>
2024-10-19 16:02:12 +0000

    arrelgo urls 2

--- ofipensiones/urls.py
+++ ofipensiones/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('users.urls')),
     path('',include('reports.urls')),
+    path('health-check/', views.healthCheck),
 ]
