639afec84c9b085f649d34b764f011a6a55d705d
I-Bonnett-I <t.bonnett@uniandes.edu.co>
2024-10-19 16:35:50 +0000

    apps.py

--- reports/apps.py
+++ reports/apps.py
@@ -1,6 +1,20 @@
+import threading
 from django.apps import AppConfig
+from .report_handler import start_report_handler, generate_reports
 
 
 class ReportsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reports'
+    def ready(self):
+        # Generar los reportes
+        generate_reports()
+
+        # Iniciar el manejador de reportes en un hilo separado
+        thread = threading.Thread(target=start_report_handler)
+        thread.daemon = True
+        thread.start()
+
+
+
+
