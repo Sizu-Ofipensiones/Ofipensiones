3fd41da2dabe31a98f091ae6e3553ac1c9078d58
Nicolas Padilla <npadilla133@gmail.com>
2024-12-04 14:12:33 -0500

    no era

--- ofipensiones/urls.py
+++ ofipensiones/urls.py
@@ -3,15 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    # Ruta para el panel de administración
     path('admin/', admin.site.urls),
-
-    # Ruta para la página de inicio
-    path('', views.home, name='home'),
-
-    # Rutas para autenticación de usuarios (login, logout, etc.)
-    path('auth/', include('django.contrib.auth.urls')),
-
-    # Rutas para autenticación con Social Auth (auth0, por ejemplo)
-    path('auth/', include('social_django.urls')),
+    path('', views.home),
+    path(r'', include('django.contrib.auth.urls')),
+    path(r'', include('social_django.urls')),
 ]
