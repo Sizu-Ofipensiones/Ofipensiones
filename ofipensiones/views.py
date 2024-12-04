7a95858b4cde1275733b449cd7e28ed6dfa50043
Nicolas Padilla <npadilla133@gmail.com>
2024-12-04 15:34:57 -0500

    probando sin rol

--- ofipensiones/views.py
+++ ofipensiones/views.py
@@ -6,4 +6,4 @@ from ofipensiones.auth0Backend import getRole
 @login_required
 def home(request):
     role = getRole(request)
-    return render(request, 'base.html', {'role': role})
+    return render(request, 'base.html')
