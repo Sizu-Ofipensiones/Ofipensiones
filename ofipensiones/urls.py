18ba5b481383a53d7a3a8e1caa83169df0d4ce4f
Nicolas Padilla <npadilla133@gmail.com>
2024-12-04 22:45:56 -0500

    dios mio tengo sueño

--- ofipensiones/urls.py
+++ ofipensiones/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('consultar_logs/', views.consultar_logs, name='consultar_logs'),
-    path('actualizar_reporte/', views.modificar_recibo, name='actualizar_reporte'),
+    path('actualizar_reporte/', views.actualizar_reporte, name='actualizar_reporte'),
     path('listar_reportes/', views.obtener_reportes, name='listar_reportes'),
     path(r'', include('django.contrib.auth.urls')),
     path(r'', include('social_django.urls')),
