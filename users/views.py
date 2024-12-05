42e099e9371cfc28de20d3654d91247ce301b44f
Nicolas Padilla <npadilla133@gmail.com>
2024-12-05 10:19:25 -0500

    logs

--- users/views.py
+++ users/views.py
@@ -1,17 +1,37 @@
 from django.http import JsonResponse
+import logging
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from users.models import User
 import json
 
+logger = logging.getLogger(__name__)
+
+
 @csrf_exempt
 @require_http_methods(["POST"])
 def create_user(request):
     """Crear un usuario en la base de datos"""
-    data = json.loads(request.body.decode('utf8'))
-    user = User(name=data['name'], email=data['email'], password=data['password'], position=data['position'])
-    user.save()
-    return JsonResponse({'message': f'Usuario {user.name} creado exitosamente.'})
+    try:
+        # Verificar el cuerpo de la solicitud
+        logger.info(f"Body de la solicitud: {request.body}")
+        
+        # Decodificar el cuerpo
+        data = json.loads(request.body.decode('utf8'))
+        logger.info(f"Datos decodificados: {data}")
+        
+        # Crear el usuario
+        user = User(name=data['name'], email=data['email'], password=data['password'], position=data['position'])
+        user.save()
+
+        return JsonResponse({'message': f'Usuario {user.name} creado exitosamente.'})
+
+    except json.JSONDecodeError as e:
+        return JsonResponse({'error': 'Error al procesar JSON: ' + str(e)}, status=400)
+    except KeyError as e:
+        return JsonResponse({'error': f"Falta el campo requerido: {str(e)}"}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': 'Error interno del servidor: ' + str(e)}, status=500)
 
 @csrf_exempt
 @require_http_methods(["PUT"])
