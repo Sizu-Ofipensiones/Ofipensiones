88513eb4e5c88a170a32c74efe7b1979856c317d
Nicolas Padilla <npadilla133@gmail.com>
2024-12-04 23:59:29 -0500

    no se que me tire

--- ofipensiones/settings.py
+++ ofipensiones/settings.py
@@ -123,17 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static', 'media')
-#MEDIA_URL = 'static/media/'
 
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Configuración de Auth0
 LOGIN_URL = "/login/auth0"
