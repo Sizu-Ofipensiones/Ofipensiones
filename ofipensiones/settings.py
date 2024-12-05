47359e1b82ca71f291c5660e9e06cc3abf07d601
Nicolas Padilla <npadilla133@gmail.com>
2024-12-05 09:21:43 -0500

    aloww

--- ofipensiones/settings.py
+++ ofipensiones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^92_+d_hx#r09ti1l*ky7_b!z&t__ya_gjwziyglck&j#+egss
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
