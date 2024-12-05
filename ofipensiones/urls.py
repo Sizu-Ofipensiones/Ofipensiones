420640495f4e51018d4d6d21a8e3841b6dfc828a
Nicolas Padilla <npadilla133@gmail.com>
2024-12-04 22:45:02 -0500

    otro error mk

--- ofipensiones/urls.py
+++ ofipensiones/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('consultar_logs/', views.consultar_logs, name='consultar_logs'),
-    path('modificar_recibo/', views.modificar_recibo, name='modificar_recibo'),
+    path('actualizar_reporte/', views.modificar_recibo, name='actualizar_reporte'),
     path('listar_reportes/', views.obtener_reportes, name='listar_reportes'),
     path(r'', include('django.contrib.auth.urls')),
     path(r'', include('social_django.urls')),
